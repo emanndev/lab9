91239dc0d0d477c96065b4fe5dfe221351fbe917
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-04 16:00:55 +0000

    Added invoice details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
 import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
+import { InvoiceDetailsComponent } from './components/invoice-details/invoice-details.component';
 
 export const routes: Routes = [
       { 
         path: 'invoices', 
         component: InvoiceListComponent 
     },
+     { 
+      path: 'invoices/:id', 
+      component: InvoiceDetailsComponent 
+
+     },
   { 
     path: '', 
     redirectTo: 'invoices', 
-    pathMatch: 'full' },
+    pathMatch: 'full' 
+  },
   { 
     path: '**', 
     redirectTo: 'invoices' 
