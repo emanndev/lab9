83436fb30344599972a20826f4d8ee14ff2d3402
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-04 22:21:02 +0000

    Implemented the Invoice details page

--- src/app/components/invoice-details/invoice-details.component.ts
+++ src/app/components/invoice-details/invoice-details.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { InvoiceService } from '../../services/invoice.service';
+import { Invoice } from '../../model/invoice.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './invoice-details.component.html',
   styleUrl: './invoice-details.component.scss'
 })
 export class InvoiceDetailsComponent {
+ invoice: Invoice | undefined;
+  confirmDelete: boolean = false;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private invoiceService: InvoiceService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) this.invoice = this.invoiceService.getInvoiceById(id);
+  }
+
+  goBack() {
+    this.router.navigate(['/invoices']);
+  }
+
+  deleteInvoice() {
+    if (this.invoice) {
+      this.invoiceService.deleteInvoice(this.invoice.id);
+      this.goBack();
+    }
+  }
+
+  markAsPaid() {
+    if (this.invoice && this.invoice.status !== 'paid') {
+      this.invoiceService.markAsPaid(this.invoice.id);
+    }
+  }
+
+  saveChanges(updated: Invoice) {
+    this.invoiceService.updateInvoice(updated);
+  }
+
+  toggleDeleteConfirm() {
+    this.confirmDelete = !this.confirmDelete;
+  }
 }
--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Invoice } from '../../model/invoice.model';
 import { InvoiceService } from '../../services/invoice.service';
 import { CommonModule } from '@angular/common'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-list',
   standalone: true,
   templateUrl: './invoice-list.component.html',
   styleUrls: ['./invoice-list.component.scss'],
-  imports: [CommonModule] 
+  imports: [CommonModule, RouterModule] 
 })
 export class InvoiceListComponent implements OnInit {
    invoices: Invoice[] = [];
